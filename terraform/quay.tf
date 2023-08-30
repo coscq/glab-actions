e8bba21cfcd5dbc059017df1cddd0eef4862e04c
jenkins <jcoscat@workenv-wsl>
2023-08-30 15:25:19 -0400

    check tf version

---
+++ terraform/quay.tf
@@ -0,0 +1,19 @@
+resource "helm_release" "quay_release" {
+  name       = "quay-registry"
+
+  repository = "https://helm.twun.io"
+  chart      = "docker-registry"
+
+  set {
+    name  = "ingress.enabled"
+    value = "true"
+  }
+  set {
+    name  = "ingress.hosts"
+    value = "docker-registry.localhost"
+  }
+  set {
+    name  = "ingress.className"
+    value = "traefik"
+  }
+}
